69d9902fc924ee9b54fb618419ebc5eb5dbe4d26
1459177541 <1459177541@qq.com>
2019-08-30 23:55:54 +0800

    学习 加密 & 解密 & NIO

--- src/codeAndDecode/EncodeAndDecode.java
+++ src/codeAndDecode/EncodeAndDecode.java
@@ -9,6 +9,7 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
 import java.security.SecureRandom;
 import java.util.Base64;
 
@@ -22,9 +23,9 @@ public class EncodeAndDecode {
 
     @Test
     void base64(){
-        String encode = Base64.getEncoder().encodeToString(SRC.getBytes());
-        LOGGER.info(encode);
-        byte[] decode = Base64.getDecoder().decode(encode.getBytes());
+        byte[] encode = Base64.getEncoder().encode(SRC.getBytes());
+        LOGGER.info("encode={}, length={}", new String(encode), encode.length);
+        byte[] decode = Base64.getDecoder().decode(encode);
         Assertions.assertArrayEquals(SRC.getBytes(), decode);
     }
 
@@ -90,6 +91,41 @@ public class EncodeAndDecode {
         cipher.init(Cipher.DECRYPT_MODE, keySpec);
         LOGGER.info("block size={}", cipher.getBlockSize());
         return cipher.doFinal(context);
+    }
+
+    @Test
+    void nio() throws Exception{
+        // fail
+        ByteBuffer src = ByteBuffer.allocate(64);
+        src.put("Test1".getBytes());
+        src.flip();
+        LOGGER.info("src={}", src);
+
+        SecretKeySpec encodeKeySpec = getKey(256, "AES", KEY.getBytes());
+        Cipher encodeCipher = Cipher.getInstance("AES");
+        encodeCipher.init(Cipher.ENCRYPT_MODE, encodeKeySpec);
+
+        ByteBuffer encode = ByteBuffer.allocate(128);
+        LOGGER.info("encode={}",encodeCipher.update(src, encode));
+        LOGGER.info("encode result={}", encode);
+        encodeCipher.doFinal();
+        LOGGER.info("encode final={}", encode);
+        encode.flip();
+
+        SecretKeySpec decodeKeySpec = getKey(256, "AES", KEY.getBytes());
+        Cipher decodeCipher = Cipher.getInstance("AES");
+        decodeCipher.init(Cipher.DECRYPT_MODE, decodeKeySpec);
+
+        ByteBuffer decode = ByteBuffer.allocate(64);
+        LOGGER.info("decode={}", decodeCipher.update(encode, decode));
+        LOGGER.info("getOutputSize={}", decodeCipher.getOutputSize(64));
+        decodeCipher.doFinal();
+
+        LOGGER.info(decode);
+        decode.flip();
+        byte[] data = new byte[decode.limit()];
+        decode.get(data);
+        LOGGER.info("decode result={}", new String(data));
     }
 
 }
--- src/nio/NIOTest.java
+++ src/nio/NIOTest.java
@@ -23,33 +23,57 @@ public class NIOTest {
         assertEquals(1024, byteBuffer.limit());
         assertEquals(0, byteBuffer.position());
         assertEquals(1024, byteBuffer.capacity());
+        assertEquals(1024, byteBuffer.remaining());
 
         byteBuffer.put("Test".getBytes());
         assertEquals(1024, byteBuffer.limit());
         assertEquals(4, byteBuffer.position());
         assertEquals(1024, byteBuffer.capacity());
+        assertEquals(1020, byteBuffer.remaining());
+
+        byteBuffer.put("Test".getBytes());
+        assertEquals(1024, byteBuffer.limit());
+        assertEquals(8, byteBuffer.position());
+        assertEquals(1024, byteBuffer.capacity());
+        assertEquals(1016, byteBuffer.remaining());
+
+        byteBuffer.flip();
+        assertEquals(8, byteBuffer.limit());
+        assertEquals(0, byteBuffer.position());
+        assertEquals(1024, byteBuffer.capacity());
+        assertEquals(8, byteBuffer.remaining());
+
+        byteBuffer.put("TEST".getBytes());
+        assertEquals(8, byteBuffer.limit());
+        assertEquals(4, byteBuffer.position());
+        assertEquals(1024, byteBuffer.capacity());
+        assertEquals(4, byteBuffer.remaining());
 
         byteBuffer.flip();
         assertEquals(4, byteBuffer.limit());
         assertEquals(0, byteBuffer.position());
         assertEquals(1024, byteBuffer.capacity());
+        assertEquals(4, byteBuffer.remaining());
 
         byte[] data = new byte[byteBuffer.limit()];
         byteBuffer.get(data);
-        assertEquals("Test", new String(data));
+        assertEquals("TEST", new String(data));
         assertEquals(4, byteBuffer.limit());
         assertEquals(4, byteBuffer.position());
         assertEquals(1024, byteBuffer.capacity());
+        assertEquals(0, byteBuffer.remaining());
 
         byteBuffer.clear();
         assertEquals(1024, byteBuffer.limit());
         assertEquals(0, byteBuffer.position());
         assertEquals(1024, byteBuffer.capacity());
+        assertEquals(1024, byteBuffer.remaining());
 
         byteBuffer.flip();
         assertEquals(0, byteBuffer.limit());
         assertEquals(0, byteBuffer.position());
         assertEquals(1024, byteBuffer.capacity());
+        assertEquals(0, byteBuffer.remaining());
     }
 
     public static void main(String[] args) throws Exception{
