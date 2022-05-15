102dbe004dd17d89e6b8a0e79d0647c73d34e490
Yang-xingchen <1459177541@qq.com>
2022-05-15 16:24:10 +0800

    Processor

---
+++ base/src/main/java/processor/Main.java
@@ -0,0 +1,10 @@
+package processor;
+
+@ProcessorClass
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+
+}
---
+++ base/src/main/java/processor/MyProcessor.java
@@ -0,0 +1,43 @@
+package processor;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@SupportedAnnotationTypes("processor.ProcessorClass")
+@SupportedSourceVersion(SourceVersion.RELEASE_16)
+public class MyProcessor extends AbstractProcessor {
+
+    public MyProcessor() {
+        System.out.println("processor...");
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Path path = Paths.get("./process.txt");
+        String collect = roundEnv.getRootElements()
+                .stream()
+                .filter(TypeElement.class::isInstance)
+                .map(element -> ((TypeElement) element).getQualifiedName())
+                .map(Objects::toString)
+                .collect(Collectors.joining("\n"));
+        try {
+            Files.writeString(path, collect, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
