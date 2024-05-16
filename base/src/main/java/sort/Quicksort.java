9bb336e9a34e93f46ace1cc82172db0abff4963e
Yang-xingchen <1459177541@qq.com>
2024-05-16 17:25:52 +0800

    快速排序

---
+++ base/src/main/java/sort/Quicksort.java
@@ -0,0 +1,66 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+public class Quicksort {
+
+    public static void main(String[] args) throws Exception {
+        int[] arr = {8, 1, 6, 7, 5, 2, 9, 4, 3};
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.submit(new Task(arr, 0, arr.length - 1)).get();
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static class Task extends RecursiveAction {
+
+        private int[] arr;
+        private int start;
+        private int end;
+
+        public Task(int[] arr, int start, int end) {
+            this.arr = arr;
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        protected void compute() {
+            int l = start;
+            int r = end;
+            boolean gt = true;
+            while (l != r) {
+                if (arr[l] > arr[r]) {
+                    int t = arr[l];
+                    arr[l] = arr[r];
+                    arr[r] = t;
+                    gt = !gt;
+                }
+                if (gt) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+            Task lt = null;
+            Task rt = null;
+            if (start < l) {
+                lt = new Task(arr, start, l - 1);
+                lt.fork();
+            }
+            if (end > r) {
+                rt = new Task(arr, r + 1, end);
+                rt.fork();
+            }
+            if (lt != null) {
+                lt.join();
+            }
+            if (rt != null) {
+                rt.join();
+            }
+        }
+
+    }
+
+}
