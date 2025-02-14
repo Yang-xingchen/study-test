c5bdf9835afd76199c535522a2b6f661db67ef25
Yang-xingchen <1459177541@qq.com>
2025-02-14 15:29:33 +0800

    pulsar: 单机安装、批量获取、手动提交

---
+++ middleware/pulsar/README.md
@@ -0,0 +1,21 @@
+# Pulsar
+[官网](https://pulsar.apache.org/)
+
+## 安装
+1. 解压
+2. 到根目录
+
+## 命令
+启动
+```shell
+bin/pulsar-daemon start standalone
+```
+关闭
+```shell
+bin/pulsar-daemon stop standalone
+```
+创建主题
+```shell
+bin/pulsar-admin topics create persistent://public/default/topic
+```
+
---
+++ middleware/pulsar/src/main/java/com/example/pulsar/PulsarApplication.java
@@ -0,0 +1,50 @@
+package com.example.pulsar;
+
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.MessageId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.pulsar.annotation.PulsarListener;
+import org.springframework.pulsar.core.PulsarTemplate;
+import org.springframework.pulsar.listener.AckMode;
+import org.springframework.pulsar.listener.Acknowledgement;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class PulsarApplication {
+
+	private Logger log = LoggerFactory.getLogger(PulsarApplication.class);
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(PulsarApplication.class, args);
+		TimeUnit.MINUTES.sleep(10);
+	}
+
+	@Bean
+	public CommandLineRunner send(PulsarTemplate<String> pulsarTemplate) {
+		return args -> {
+			for (int i = 0; i < 10; i++) {
+				pulsarTemplate.newMessage("spring: " + i)
+						.withTopic("topic")
+						.send();
+			}
+		};
+	}
+
+	@PulsarListener(topics = "topic",
+			subscriptionName = "spring-test",
+			ackMode = AckMode.MANUAL,
+			batch = true)
+	public void listener(List<Message<String>> message, Acknowledgement acknowledgement) {
+		log.info("listener: {}", message.stream().map(Message::getValue).toList());
+		List<MessageId> ids = message.stream().map(Message::getMessageId).toList();
+		acknowledgement.acknowledge(ids);
+	}
+
+}
